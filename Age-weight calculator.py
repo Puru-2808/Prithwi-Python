ef158ebc258ab654f221c77f9712ef1d81f3edaf
Puru-2808 <purusottamrooj2021@gmail.com>
2025-07-29 17:12:31 +0530

    error correction

--- Age-weight calculator.py
+++ Age-weight calculator.py
@@ -13,21 +13,31 @@ while True:
 
     d=(year*12*30)+(month*30)+day
     
-    if d<=90:
-     w1=(2800+(25*d))/1000
-     w2=(3000+(25*d))/1000
-     print("Minimum weight: ",w1,"Kg")
-     print("Maximum weight: ",w2,"Kg")
-    elif d<=720:
-      w1=(2800+(25*90)+(15*(d-90)))/1000
-      w2=(3000+(25*90)+(15*(d-90)))/1000
+    if d<=90:                         # 0 to 3 months
+        w1=(2500+(25*d))/1000
+        w2=(3000+(25*d))/1000
+        print("Minimum weight: ",w1,"Kg")
+        print("Maximum weight: ",w2,"Kg")
+    elif d<=150:                            # by 5 months
+      w1=(2800*2)/1000
+      w2=(3000*2)/1000
       print("Minimum weight: ",w1,"Kg")
       print("Maximum weight: ",w2,"Kg")
-    elif d<=4320:
-      w1=(2800+(25*90)+(15*630)+((2500/360)*(d-720)))/1000
-      w2=(3000+(25*90)+(15*630)+((3000/360)*(d-720)))/1000
+    elif d<=360:                           # by 1 year
+      w1=(2800*3)/1000
+      w2=(3000*3)/1000
       print("Minimum weight: ",w1,"Kg")
       print("Maximum weight: ",w2,"Kg")
+    elif d<=720:                           # by 2 years
+       w1=(2800*4)/1000
+       w2=(3000*4)/1000
+       print("Minimum weight: ",w1,"Kg")
+       print("Maximum weight: ",w2,"Kg")
+    elif d<=(12*12*30):                    # after 2 years upto 12 years
+       w1=((2800*4)+(2500*((d-720)/(30*12))))/1000
+       w2=((3000*4)+(3000*((d-720)/(30*12))))/1000
+       print("Minimum weight: ",w1,"Kg")
+       print("Maximum weight: ",w2,"Kg")
     else:
       print("Invalid input")
     q=str(input("Do you want to continue?(yes/no): "))

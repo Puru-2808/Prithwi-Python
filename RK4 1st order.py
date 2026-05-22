82bf8ef301648e8a826f831cefd3104db760edfc
Puru-2808 <purusottamrooj2021@gmail.com>
2026-05-22 22:25:12 +0530

    adding

---
+++ RK4 1st order.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def f(x,y):
+    return 5*y*math.cos(4*x)
+
+xi,xf=0,2*math.pi
+yi=0.5
+n=1001
+h=(xf-xi)/(n-1)
+xx=[]
+yy=[]
+xx.append(xi)
+yy.append(yi)
+for i in range(n):
+    k1=f(xi,yi)
+    k2=f(xi+(h/2),yi+k1*(h/2))
+    k3=f(xi+(h/2),yi+k2*(h/2))
+    k4=f(xi+h,yi+k3*h)
+    k=(k1+2*k2+2*k3+k4)/6
+    x=xi+h
+    y=yi+k*h
+    xi=x
+    yi=y
+    xx.append(x)
+    yy.append(y)
+
+xx1=[]
+yy1=[]
+for i in range(n):
+    x1=xi+i*h
+    y1=0.5*math.exp((5/4)*math.sin(4*x1))
+    xx1.append(x1)
+    yy1.append(y1)
+
+plt.plot(xx,yy)
+plt.plot(xx1,yy1)
+plt.show()

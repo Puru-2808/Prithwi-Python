e8c51b4e15db786c585cb2f58449edba9cff6848
Puru-2808 <purusottamrooj2021@gmail.com>
2026-05-23 22:23:17 +0530

    adding

--- Moving Projectile Motion.py
+++ Moving Projectile Motion.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # initial value of states position and velocity
-angle=0
+angle=90
 theta=(np.pi/180)*angle
-u=50  
-g=9.8
-h=1.651
-dt=0.01
+u=(11.2)*1000
+G=6.674*10**(-11)
+M=5.972*10**(24)
+Re=6.371*10**6
+g=G*M/(Re**2)
+h=0
+dt=1
 x=0
 y=h
 t=0
@@ -22,14 +25,15 @@ vy=u*np.sin(theta)
 fig,ax=plt.subplots()
 line,=ax.plot([],[],'r-')
 point,=ax.plot([],[],'bo')
-ax.set_xlim(0,100)
-ax.set_ylim(0,200)
+ax.set_xlim(0,1000000)
+ax.set_ylim(0,2000000)
 
 
 def update(frame):
-    global x,y,vx,vy,t
+    global x,y,vx,vy,g,t
     x=x+vx*dt
     y=y+vy*dt
+    g=G*M/(Re+y)**2
     vy=vy-g*dt
     t=t+dt
     xt.append(x)
@@ -40,7 +44,7 @@ def update(frame):
     R=xt[-1]
     H=yt[-1]
     T=tt[-1]
-    if y<=0.001:
+    if y<=0.001 and t>0:
         print("Time of flight:",T)
         print("Range:",R)
         print("Max height:",max(yt))
